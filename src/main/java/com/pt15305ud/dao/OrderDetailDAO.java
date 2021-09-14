d51d3f0ff4597d9a94dee4ae95d35d4c58b3f168
kazuokoko <be.myself.1994.ls@gmail.com>
2021-09-14 21:19:53 +0700

    123
    12

--- src/main/java/com/pt15305ud/dao/OrderDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.pt15305ud.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pt15305ud.entity.OrderDetail;
-
-public interface OrderDetailDAO extends JpaRepository<OrderDetail, Long> {
-
-}
